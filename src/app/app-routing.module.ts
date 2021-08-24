4b56152899dfbbd373cfd9385af078be7088549c
weidehai <243395655@qq.com>
2021-08-24 20:24:14 +0800

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EchartDemoComponent } from './echart-demo/echart-demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'echart-demo', component: EchartDemoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/echart-demo/echart-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-echart-demo',
+  templateUrl: './echart-demo.component.html',
+  styleUrls: ['./echart-demo.component.scss']
+})
+export class EchartDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
