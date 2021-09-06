314f22d332dd09ca8261b36d754540a878c42449
weidehai <243395655@qq.com>
2021-09-06 19:48:01 +0800

    practice

--- src/app/echart-demo/echart-demo.component.ts
+++ src/app/echart-demo/echart-demo.component.ts
@@ -141,56 +141,298 @@ function dataZoom() {
     ],
   });
 }
-function dataZoom1() {
-  const myChart = echarts.init(
-    <HTMLElement>document.querySelector('.data-zoom1')
-  );
+
+function pic1() {
+  const myChart = echarts.init(<HTMLElement>document.querySelector('.pictrue'));
   myChart.setOption({
-    xAxis: {
-      type: 'value',
+    title: {
+      text: '孤帆远影碧空尽,唯见长江天际流',
     },
-    yAxis: {
-      type: 'value',
+    grid: {
+      left: 0,
+      right: 0,
+      bottom: 0,
     },
-    dataZoom: [
+    backgroundColor: {
+      type: 'radial',
+      x: 0.62,
+      y: 0.45,
+      r: 1,
+      colorStops: [
+        {
+          offset: 0,
+          color: '#fe9e80', // 0% 处的颜色
+        },
+        {
+          offset: 1,
+          color: '#9bc9f1', // 100% 处的颜色
+        },
+      ],
+      global: false, // 缺省为 false
+    },
+    xAxis: [
       {
-        // 这个dataZoom组件，默认控制x轴。
-        type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
-        start: 10, // 左边在 10% 的位置。
-        end: 60, // 右边在 60% 的位置。
+        data: new Array(15),
+        boundaryGap: false,
+        show: false,
       },
+    ],
+    yAxis: [
       {
-        // 这个dataZoom组件，也控制x轴。
-        type: 'inside', // 这个 dataZoom 组件是 inside 型 dataZoom 组件
-        start: 10, // 左边在 10% 的位置。
-        end: 60, // 右边在 60% 的位置。
+        min: 0,
+        max: 10,
+        show: false,
       },
     ],
     series: [
       {
-        type: 'scatter', // 这是个『散点图』
-        itemStyle: {
+        type: 'line',
+        areaStyle: {
+          color: '#2a9c91',
+          opacity: 0.4,
+        },
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        data: [5, 5, 5, 6.1, 6.2, 6.3, 6.4, 6, 4, 4, 4, 4, 4, 4, 4],
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#2a9c91',
           opacity: 0.8,
         },
-        symbolSize: function (val: any) {
-          return val[2] * 40;
+        data: [4, 4, 4, 4, 4, 4, 4, 4, 3, 4.5, 5, 6, 6.5, 7, 7.2],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#2dc3b5',
+          opacity: 1,
+        },
+        data: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#0f6960',
+          opacity: 1,
+        },
+        data: [6.5, 6, 6, 6, 5.8, 5.2, 4.8, 4.2, 2, 0, 0, 0, 0, 0, 0],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#2dc3b5',
+          opacity: 1,
         },
         data: [
-          ['14.616', '7.241', '0.896'],
-          ['3.958', '5.701', '0.955'],
-          ['2.768', '8.971', '0.669'],
-          ['9.051', '9.710', '0.171'],
-          ['14.046', '4.182', '0.536'],
-          ['12.295', '1.429', '0.962'],
-          ['4.417', '8.167', '0.113'],
-          ['0.492', '4.771', '0.785'],
-          ['7.632', '2.605', '0.645'],
-          ['14.242', '5.042', '0.368'],
+          2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8, 2.8,
+          2.8,
         ],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#0f6960',
+          opacity: 1,
+        },
+        data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 4.8, 5, 5.5],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'line',
+        areaStyle: {
+          color: '#2dc3b5',
+          opacity: 0.8,
+        },
+        data: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        symbolSize: 0,
+        lineStyle: {
+          width: 0,
+        },
+        silent: true,
+      },
+      {
+        type: 'graph',
+        layout: 'none',
+        data: [
+          {
+            x: 0,
+            y: 0,
+            symbolSize: 100,
+          },
+        ],
+        itemStyle: {
+          color: '#ff5722',
+          shadowColor: '#ff5722',
+          shadowBlur: 100,
+        },
+        silent: true,
+        left: '35%',
+        top: '6%',
+        z: 3,
+      },
+      {
+        type: 'graph',
+        layout: 'none',
+        data: [
+          {
+            x: 0,
+            y: 0,
+            symbolSize: 50,
+          },
+        ],
+        itemStyle: {
+          color: '#333',
+        },
+        silent: true,
+        left: '38%',
+        top: '25%',
+        z: 3,
+        symbol:
+          'path://M27.310007,2.749997l22.5,0c-2.485281,0 -4.5,14.326891 -4.5,32.000002c0,17.673113 2.014718,32 4.5,32l-22.5,0l0,0c-2.485281,0 -4.5,-14.326889 -4.5,-32c0,-17.673111 2.014718,-32.000002 4.5,-32.000002z M12.75,70.184998l47,0l-11,10l-30,-1l-6,-9z',
+      },
+      {
+        //birds
+        type: 'graph',
+        data: [
+          //bg
+          {
+            symbolSize: 0,
+            x: 0,
+            y: 0,
+          },
+          {
+            symbolSize: 0,
+            x: 200,
+            y: 100,
+          },
+          //bird1
+          {
+            symbolSize: 0,
+            x: 120,
+            y: 50,
+            name: 'first-bird-left',
+          },
+          {
+            symbolSize: 3,
+            x: 125,
+            y: 52,
+            name: 'first-bird',
+          },
+          {
+            symbolSize: 0,
+            x: 130,
+            y: 50,
+            name: 'first-bird-right',
+          },
+          //bird2
+          {
+            symbolSize: 0,
+            x: 110,
+            y: 43,
+            name: 'second-bird-left',
+          },
+          {
+            symbolSize: 2,
+            x: 115,
+            y: 45,
+            name: 'second-bird',
+          },
+          {
+            symbolSize: 0,
+            x: 120,
+            y: 43,
+            name: 'second-bird-right',
+          },
+          //bird3
+          {
+            symbolSize: 0,
+            x: 112,
+            y: 52,
+            name: 'third-bird-left',
+          },
+          {
+            symbolSize: 2,
+            x: 115,
+            y: 53,
+            name: 'third-bird',
+          },
+          {
+            symbolSize: 0,
+            x: 118,
+            y: 52,
+            name: 'third-bird-right',
+          },
+        ],
+        links: [
+          {
+            source: 'first-bird-left',
+            target: 'first-bird',
+          },
+          {
+            source: 'first-bird',
+            target: 'first-bird-right',
+          },
+          {
+            source: 'second-bird-left',
+            target: 'second-bird',
+          },
+          {
+            source: 'second-bird',
+            target: 'second-bird-right',
+          },
+          {
+            source: 'third-bird-left',
+            target: 'third-bird',
+          },
+          {
+            source: 'third-bird',
+            target: 'third-bird-right',
+          },
+        ],
+        lineStyle: {
+          width: 1,
+          curveness: 0.3,
+          color: '#333',
+        },
+        itemStyle: {
+          color: '#555',
+        },
+        silent: true,
+        z: 4,
       },
     ],
   });
 }
+
 @Component({
   selector: 'app-echart-demo',
   templateUrl: './echart-demo.component.html',
@@ -202,6 +444,6 @@ export class EchartDemoComponent implements OnInit {
     bar();
     nantuta();
     dataZoom();
-    dataZoom1()
+    pic1();
   }
 }
